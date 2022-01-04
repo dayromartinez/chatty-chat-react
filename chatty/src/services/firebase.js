0fafec59ab01a93f2999876630f37145d7668083
Dayro Martínez <djmd96@hotmail.com>
2022-01-04 01:40:38 -0500

    Chatty deploy Firebase OK!

--- chatty/src/services/firebase.js
+++ chatty/src/services/firebase.js
@@ -4,12 +4,12 @@ import "firebase/database";
 const config = {
   apiKey: "AIzaSyC8FYGzZCU3X2vLYIZ4zHR87yViZR_Xuow",
   authDomain: "chatty-chat-react.firebaseapp.com",
+  databaseURL: "https://chatty-chat-react-default-rtdb.firebaseio.com",
   projectId: "chatty-chat-react",
   storageBucket: "chatty-chat-react.appspot.com",
   messagingSenderId: "291081322081",
   appId: "1:291081322081:web:da10aca5eb216cd701f420",
-  measurementId: "G-CX29Q38MJ7",
-  databaseURL: "https://chatty-chat-react-default-rtdb.firebaseio.com"
+  measurementId: "G-CX29Q38MJ7"
 };
 
 firebase.initializeApp(config);
